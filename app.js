f43202cb519f97bc3a986af527e6c0a848279348
A-Gaborit <audrey.gaborit85@gmail.com>
2025-07-01 09:10:42 +0200

    add: service worker

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+  .then((reg) => {
+    // registration worked
+    console.log('Enregistrement réussi');
+  }).catch((error) => {
+    // registration failed
+    console.log('Erreur : ' + error);
+  });
+}
+
 let people = JSON.parse(localStorage.getItem("scienceData")) || [];
 
 function addPerson() {
