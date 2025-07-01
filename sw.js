4ee3a4cff4bad078e6e58345c2550d83960eadb3
JulienPajot <julien.pajot@dupdevinci-edu.fr>
2025-07-01 10:37:16 +0200

    add storage cache

--- sw.js
+++ sw.js
@@ -1,7 +1,27 @@
 // <!-- Écouter l'installation du SW -->
- 
+const CACHE_NAME = 'science-cacahe'; // nom du cache
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/open.html',
+  '/random.html',
+  '/share.html',
+  '/style.css',
+  '/app.js',
+  '/sw.js',
+  '/manifest.json',
+  '/assets/manifest-icon-192.maskable.png',
+  'assets/manifest-icon-512.maskable.png'
+];
+
 self.addEventListener('install', event => { // indice: quand le SW est installé
   console.log(' Service Worker installé');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Mise en cache des fichiers :', FILES_TO_CACHE);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
   self.skipWaiting(); // indice: forcer à prendre le contrôle immédiatement
 });
  
@@ -10,4 +30,4 @@ self.addEventListener('activate', event => { // indice: quand le SW devient acti
   console.log(' Service Worker activé');
   self.clients.claim(); // indice: prendre le contrôle des pages ouvertes
 });
- 
+
